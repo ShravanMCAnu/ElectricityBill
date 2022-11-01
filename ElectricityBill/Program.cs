efa5c7ff471e9debc27b59a3a0ae984f2080b834
ShravanMCAnu <bskmc850@gmail.com>
2022-11-01 15:37:36 +0530

    Add project files.

---
+++ ElectricityBill/Program.cs
@@ -0,0 +1,12 @@
+﻿using ElectricityBill;
+
+UnitAmount ua=new UnitAmount();
+Console.WriteLine("Enter units for Electricity Bill");
+double eUnits = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter you service type, if domestict say 'y' or 'Y', if not say 'n' or 'N': ");
+string serviceType =Console.ReadLine();
+ua.UnitCal(eUnits, serviceType);
+
+
+Console.WriteLine("Bill Amount Bill is:{0}", ua.BillAmount.ToString("0.00"));
---
+++ ElectricityBill/UnitAmount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricityBill
+{
+    public class UnitAmount
+    {
+        public double BillAmount { get; set; }
+        public void UnitCal(double _units, string _domestic)
+        {
+            if (_domestic == "y" || _domestic == "Y")
+            {
+                if (_units > 200)
+                {
+                    BillAmount = _units * 1.3;
+                }
+                else if (_units > 400)
+                {
+                    BillAmount = _units * 2.4;
+                }
+                else if (_units > 600)
+                {
+                    BillAmount = _units * 3.6;
+                }
+                else
+                {
+                    BillAmount = _units * 0.8;
+                }
+            }
+            else
+            {
+                if (_units > 200)
+                {
+                    BillAmount = _units * 2;
+                }
+                else if (_units > 400)
+                {
+                    BillAmount = _units * 3.2;
+                }
+                else if (_units > 600)
+                {
+                    BillAmount = _units * 5.1;
+                }
+                else
+                {
+                    BillAmount = _units * 1.3;
+                }
+            }
+        }
+    }
+}
